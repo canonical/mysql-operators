ea9442eaa02efe6ac8b2cc735146154abbe32e00
Sinclert Pérez <sinclert.perez@canonical.com>
2026-02-13 14:01:10 +0100

    [MISC] 8.0 - Bump mysql-shell-client to >=0.7.1 (#80)

--- kubernetes/lib/charms/mysql/v0/async_replication.py
+++ kubernetes/lib/charms/mysql/v0/async_replication.py
@@ -59,7 +59,7 @@ LIBID = "4de21f1a022c4e2c87ac8e672ec16f6a"
 LIBAPI = 0
 LIBPATCH = 12
 
-PYDEPS = ["mysql_shell_client ~= 0.6"]
+PYDEPS = ["mysql_shell_client ~= 0.7"]
 
 RELATION_OFFER = "replication-offer"
 RELATION_CONSUMER = "replication"
