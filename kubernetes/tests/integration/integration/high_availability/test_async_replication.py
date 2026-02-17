d3b3c116bd800055521d9141357ea58553631bbf
Sinclert Pérez <sinclert.perez@canonical.com>
2026-02-17 10:38:54 +0100

    Switch to mysql-router with 24.04 base

--- kubernetes/tests/integration/integration/high_availability/test_async_replication.py
+++ kubernetes/tests/integration/integration/high_availability/test_async_replication.py
@@ -141,8 +141,8 @@ def test_deploy_router_and_app(first_model: str) -> None:
     model_1.deploy(
         charm=MYSQL_ROUTER_NAME,
         app=MYSQL_ROUTER_NAME,
-        base="ubuntu@22.04",
-        channel="8.0/edge",
+        base="ubuntu@24.04",
+        channel="8.4/edge",
         num_units=1,
         trust=True,
     )
