ea9b51bf094992b0e61d7e76049bbdb4f11dc466
Raul Almeida <raul.almeida@canonical.com>
2026-03-12 10:07:59 -0300

    enh: mocks from decorator in machine tests

--- machines/tests/unit/test_async_replication.py
+++ machines/tests/unit/test_async_replication.py
@@ -509,31 +509,23 @@ class TestAsyncRelation(unittest.TestCase):
 
         _mysql.rejoin_cluster.assert_called_once_with(self.charm.app_peer_data["cluster-name"])
 
-    def test_consumer_relation_created_deferred_when_peer_data_not_set(self, _):
+    @patch("charm.MySQLOperatorCharm._is_peer_data_set", new_callable=PropertyMock, return_value=False)
+    @patch("ops.framework.EventBase.defer")
+    def test_consumer_relation_created_deferred_when_peer_data_not_set(self, _, mock_defer, mock_peer_data):
         """Test that consumer relation created event is deferred when peer data is not set."""
         self.harness.set_leader(True)
-        with (
-            patch.object(
-                type(self.harness.charm), "_is_peer_data_set", property(lambda self: False)
-            ),
-            patch("ops.framework.EventBase.defer") as mock_defer,
-        ):
-            relation_id = self.harness.add_relation("replication", "remote")
-            self.harness.add_relation_unit(relation_id, "remote/0")
+        relation_id = self.harness.add_relation("replication", "remote")
+        self.harness.add_relation_unit(relation_id, "remote/0")
 
-            mock_defer.assert_called()
+        mock_defer.assert_called()
 
-    def test_consumer_relation_changed_deferred_when_peer_data_not_set(self, _):
+    @patch("charm.MySQLOperatorCharm._is_peer_data_set", new_callable=PropertyMock, return_value=False)
+    @patch("ops.framework.EventBase.defer")
+    def test_consumer_relation_changed_deferred_when_peer_data_not_set(self, _, mock_defer, mock_peer_data):
         """Test that consumer relation changed event is deferred when peer data is not set."""
         self.harness.set_leader(True)
-        with (
-            patch.object(
-                type(self.harness.charm), "_is_peer_data_set", property(lambda self: False)
-            ),
-            patch("ops.framework.EventBase.defer") as mock_defer,
-        ):
-            relation_id = self.harness.add_relation("replication", "remote")
-            self.harness.add_relation_unit(relation_id, "remote/0")
-            self.harness.update_relation_data(relation_id, "remote/0", {"key": "value"})
+        relation_id = self.harness.add_relation("replication", "remote")
+        self.harness.add_relation_unit(relation_id, "remote/0")
+        self.harness.update_relation_data(relation_id, "remote/0", {"key": "value"})
 
-            mock_defer.assert_called()
+        mock_defer.assert_called()
