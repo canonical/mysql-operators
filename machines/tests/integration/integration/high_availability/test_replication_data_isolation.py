c1b94f0c75d5612cdfd13d259cf2cb738652e301
Paulo Machado <paulo.machado@canonical.com>
2026-04-20 11:12:56 +0200

    test revision for test-app

--- machines/tests/integration/integration/high_availability/test_replication_data_isolation.py
+++ machines/tests/integration/integration/high_availability/test_replication_data_isolation.py
@@ -36,7 +36,7 @@ def test_deploy_highly_available_cluster(juju: Juju, charm: str) -> None:
         charm=MYSQL_TEST_APP_NAME,
         app=MYSQL_TEST_APP_NAME,
         base="ubuntu@22.04",
-        channel="latest/edge",
+        channel="latest/edge/racing",
         config={"sleep_interval": 500},
         num_units=1,
     )
--- machines/tests/integration/integration/test_subordinate_charms.py
+++ machines/tests/integration/integration/test_subordinate_charms.py
@@ -28,7 +28,7 @@ def test_ubuntu_pro(juju: Juju, charm):
     juju.deploy(
         APPLICATION_APP_NAME,
         APPLICATION_APP_NAME,
-        channel="latest/edge",
+        channel="latest/edge/racing",
         base="ubuntu@22.04",
     )
     juju.deploy(
