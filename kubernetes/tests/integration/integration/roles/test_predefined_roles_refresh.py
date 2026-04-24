ea661be06c5761074785597d9bf052a48d0e28ee
Paulo Machado <paulo.machado@canonical.com>
2026-04-24 09:04:44 +0200

    Revert "test revision for test-app"
    This reverts commit 8ba74907f9ca9ebda22905ac59de1672c41dad98.

--- kubernetes/tests/integration/integration/roles/test_predefined_roles_refresh.py
+++ kubernetes/tests/integration/integration/roles/test_predefined_roles_refresh.py
@@ -179,7 +179,7 @@ def test_integrate_mysql_router_and_test_app(juju: Juju):
         charm=MYSQL_TEST_APP_NAME,
         app=MYSQL_TEST_APP_NAME,
         base="ubuntu@22.04",
-        channel="latest/edge/racing",
+        channel="latest/edge",
         num_units=1,
         config={"database_name": "test"},
     )
