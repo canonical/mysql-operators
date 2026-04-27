8f3b80ba85b32884ef285fc6a767ab47b884f99c
Juan Luis Cano Rodríguez <juan.rodriguez@canonical.com>
2026-04-27 11:19:21 +0200

    Improve observability of failed mysqlsh commands

--- kubernetes/src/mysql_k8s_executor.py
+++ kubernetes/src/mysql_k8s_executor.py
@@ -45,13 +45,13 @@ class ContainerExecutor(BaseExecutor):
                 f"--user={self._conn_details.username}",
             ]
 
-    def _parse_error(self, output: str) -> dict:
+    def _parse_exception(self, exc: ops.pebble.ExecError) -> str:
         """Parse the execution error."""
-        error = next(self._iter_output(output, "error"), None)
+        error = next(self._iter_output(exc.stdout or "", "error"), None)
         if not error:
-            error = {}
+            error = exc.stderr
 
-        return error
+        return error or ""
 
     def _parse_output_py(self, output: str) -> str:
         """Parse the Python execution output."""
@@ -117,7 +117,7 @@ class ContainerExecutor(BaseExecutor):
             )
             process.wait_output()
         except ops.pebble.ExecError as exc:
-            err = self._parse_error(exc.stdout)
+            err = self._parse_exception(exc)
             raise ExecutionError(err) from exc
         except ops.pebble.TimeoutError as exc:
             raise ExecutionError() from exc
@@ -153,7 +153,7 @@ class ContainerExecutor(BaseExecutor):
             )
             stdout, _ = process.wait_output()
         except ops.pebble.ExecError as exc:
-            err = self._parse_error(exc.stdout)
+            err = self._parse_exception(exc)
             raise ExecutionError(err) from exc
         except ops.pebble.TimeoutError as exc:
             raise ExecutionError() from exc
@@ -186,7 +186,7 @@ class ContainerExecutor(BaseExecutor):
             )
             stdout, _ = process.wait_output()
         except ops.pebble.ExecError as exc:
-            err = self._parse_error(exc.stdout)
+            err = self._parse_exception(exc)
             exc = self._strip_password(exc)
             raise ExecutionError(err) from exc
         except ops.pebble.TimeoutError as exc:
