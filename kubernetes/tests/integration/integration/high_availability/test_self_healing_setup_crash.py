bba936ae506d4ed2e6d401aa93fdde5578d048d4
Juan Luis Cano Rodríguez <juan.rodriguez@canonical.com>
2026-04-28 10:34:23 +0200

    Simplify self-healing crash test

--- kubernetes/tests/integration/integration/high_availability/test_self_healing_setup_crash.py
+++ kubernetes/tests/integration/integration/high_availability/test_self_healing_setup_crash.py
@@ -2,7 +2,6 @@
 # See LICENSE file for licensing details.
 
 import logging
-import os
 
 import jubilant
 from jubilant import Juju
@@ -11,17 +10,16 @@ from ...helpers_ha import (
     CHARM_METADATA,
     delete_k8s_pod,
     get_mysql_primary_unit,
-    load_mysql_test_data,
     update_interval,
     wait_for_apps_status,
-    wait_for_unit_status,
+    wait_for_unit_message,
 )
 
 MYSQL_APP_NAME = "mysql-k8s"
 MINUTE_SECS = 60
 
 
-def test_deploy_single_unit_cluster(juju: Juju, charm: str) -> None:
+def test_deploy_and_crash_during_cluster_setup_and_recover(juju: Juju, charm: str) -> None:
     """Simple test to ensure that the MySQL and application charms get deployed."""
     logging.info("Deploying MySQL cluster")
     juju.deploy(
@@ -30,45 +28,23 @@ def test_deploy_single_unit_cluster(juju: Juju, charm: str) -> None:
         base="ubuntu@24.04",
         config={"profile": "testing"},
         resources={"mysql-image": CHARM_METADATA["resources"]["mysql-image"]["upstream-source"]},
-        num_units=1,
+        num_units=3,
         trust=True,
     )
 
-    logging.info("Wait for applications to become active")
-    juju.wait(
-        ready=wait_for_apps_status(jubilant.all_active, MYSQL_APP_NAME),
-        error=jubilant.any_blocked,
-        timeout=20 * MINUTE_SECS,
-    )
-
-    if path := os.getenv("DATA_SOURCE_PATH"):
-        logging.info("Loading test database")
-        load_mysql_test_data(juju, MYSQL_APP_NAME, path)
-
-
-def test_crash_during_cluster_setup(juju: Juju, charm: str) -> None:
-    """Test primary crash during startup.
-
-    It must recover/end setup when the primary got offline.
-    """
-    mysql_primary = get_mysql_primary_unit(juju, MYSQL_APP_NAME)
-
-    # NOTE: This is prone to race conditions:
-    # if the units clear the "waiting" phase too quickly,
-    # this status function will never activate
-    logging.info("Scaling to 3 units")
-    juju.add_unit(MYSQL_APP_NAME, num_units=2)
+    logging.info("Wait for the first unit to be configured as primary")
     juju.wait(
         ready=lambda status: any((
             *(
-                wait_for_unit_status(MYSQL_APP_NAME, unit_name, "waiting")(status)
+                wait_for_unit_message(MYSQL_APP_NAME, unit_name, "Primary")(status)
                 for unit_name in status.get_units(MYSQL_APP_NAME)
             ),
         )),
         error=jubilant.any_blocked,
         timeout=20 * MINUTE_SECS,
-        successes=1,
     )
+
+    mysql_primary = get_mysql_primary_unit(juju, MYSQL_APP_NAME)
 
     logging.info("Deleting pod")
     delete_k8s_pod(juju, mysql_primary)
