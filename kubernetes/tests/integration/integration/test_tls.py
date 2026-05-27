42831023c6dc4db316716d2387bf026dccc332e1
Sinclert Pérez <sinclert.perez@canonical.com>
2026-05-27 09:45:54 +0200

    [MISC] Centralize relation-data test helper (#314)

--- kubernetes/tests/integration/integration/test_tls.py
+++ kubernetes/tests/integration/integration/test_tls.py
@@ -20,7 +20,7 @@ from ..helpers_ha import (
     get_app_units,
     get_mysql_server_credentials,
     get_unit_address,
-    get_unit_info,
+    get_unit_relation_data,
     wait_for_apps_status,
 )
 
@@ -135,7 +135,7 @@ def test_enable_tls(juju: Juju) -> None:
 
     # test for ca presence in a given unit
     logger.info("Assert TLS file exists")
-    assert get_tls_ca(juju, app_units[0]), "❌ No CA found after TLS relation"
+    assert get_unit_certificates_ca(juju, app_units[0], "certificates")
 
 
 def test_rotate_tls_key(juju: Juju) -> None:
@@ -210,27 +210,23 @@ def test_disable_tls(juju: Juju) -> None:
         )
 
 
-def get_tls_ca(juju: Juju, unit_name: str) -> str:
+def get_unit_certificates_ca(juju: Juju, unit_name: str, relation_name: str) -> str:
     """Returns the TLS CA used by the unit.
 
     Args:
         juju: The Juju instance
         unit_name: The name of the unit
+        relation_name: name of the relation to get data from
 
     Returns:
         TLS CA or an empty string if there is no CA.
     """
-    unit_info = get_unit_info(juju, unit_name)
-    if not unit_info:
-        raise ValueError(f"no unit info could be grabbed for {unit_name}")
+    relation_data = get_unit_relation_data(juju, unit_name, relation_name)
+    relation_fields = json.loads(relation_data["application-data"]["certificates"])
+    if not relation_fields:
+        raise ValueError(f"No relation data could be grabbed for relation {relation_name}")
 
-    # Filter the data based on the relation name.
-    relation_data = [
-        v for v in unit_info[unit_name]["relation-info"] if v["endpoint"] == "certificates"
-    ]
-    if len(relation_data) == 0:
-        return ""
-    return json.loads(relation_data[0]["application-data"]["certificates"])[0].get("ca")
+    return relation_fields[0].get("ca", "")
 
 
 def unit_file_md5(juju: Juju, unit_name: str, file_path: str) -> str | None:
