8349d5935a91568d54a9565ca3095f8bcd0880f7
Sinclert Pérez <sinclert.perez@canonical.com>
2026-06-02 11:13:53 +0200

    [VM] Define services.observers package

---
+++ machines/src/services/observers/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2026 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+from .ip_address_observer import IPAddressObserver
+from .log_rotate_observer import RotateMySQLLogsObserver
