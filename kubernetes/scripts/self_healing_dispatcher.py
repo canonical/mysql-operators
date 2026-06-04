a22e7dc9db329e9e961d6f863da52340f1de959a
Sinclert Pérez <sinclert.perez@canonical.com>
2026-06-04 11:36:32 +0200

    [K8s] Define self-healing service

---
+++ kubernetes/scripts/self_healing_dispatcher.py
@@ -0,0 +1,48 @@
+# Copyright 2026 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Self healing event dispatcher."""
+
+import argparse
+import shutil
+import subprocess
+import time
+
+
+def dispatch(unit: str, charm_directory: str):
+    """Dispatch custom event to flush mysql logs."""
+    dispatch_sub_command = f"{charm_directory}/dispatch"
+
+    juju_run = shutil.which("juju-run")
+    juju_exec = shutil.which("juju-exec")
+    command = juju_exec or juju_run or ""
+
+    subprocess.run(  # noqa: S603
+        [
+            command,
+            "-u",
+            unit,
+            "JUJU_DISPATCH_PATH=hooks/heal_mysql_cluster",
+            dispatch_sub_command,
+        ],
+        check=True,
+    )
+
+
+def main():
+    """Main watch and dispatch loop.
+
+    Roughly every 120s, dispatch the custom self_heal_mysql event.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("unit", help="name of unit")
+    parser.add_argument("charm_directory", help="base directory of the charm")
+    arguments = parser.parse_args()
+
+    while True:
+        dispatch(arguments.unit, arguments.charm_directory)
+        time.sleep(120)
+
+
+if __name__ == "__main__":
+    main()
