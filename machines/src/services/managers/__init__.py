954db84868b4228ab903bec5924f4a71422fdcef
Sinclert Pérez <sinclert.perez@canonical.com>
2026-06-04 11:57:56 +0200

    [VM] Define self-healing service

--- machines/src/services/managers/__init__.py
+++ machines/src/services/managers/__init__.py
@@ -2,7 +2,9 @@
 # See LICENSE file for licensing details.
 
 from .ip_address_manager import IPAddressManager
+from .self_healing_manager import SelfHealingManager
 
 __all__ = [
     "IPAddressManager",
+    "SelfHealingManager",
 ]
