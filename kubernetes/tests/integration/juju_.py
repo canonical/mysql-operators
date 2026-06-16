9966d7a3582d8979e37f4bd10fc3c8bd1faaf85a
Paulo Machado <paulo.machado@canonical.com>
2026-06-16 20:18:15 -0300

    use jubilant for version querying

--- kubernetes/tests/integration/juju_.py
+++ kubernetes/tests/integration/juju_.py
@@ -1,13 +1,9 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import importlib.metadata
+import jubilant_backports
 
-import ops
+juju = jubilant_backports.Juju()
 
-# libjuju version != juju agent version, but the major version should be identical—which is good
-# enough to check for secrets
-_libjuju_version = importlib.metadata.version("juju")
-has_secrets = ops.JujuVersion(_libjuju_version).has_secrets
-
-juju_major_version = int(_libjuju_version.split(".")[0])
+has_secrets = not juju._is_juju_2
+juju_major_version = 3 if has_secrets else 2
