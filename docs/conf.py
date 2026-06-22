75fc1c8faaaa43c494869c11ad4af91084363f60
a-velasco <andreia.velasco@canonical.com>
2026-06-22 14:49:33 +0200

    fix

--- docs/conf.py
+++ docs/conf.py
@@ -186,7 +186,7 @@ slug = 'data/mysql/docs'
 
 # Use RTD canonical URL to ensure duplicate pages have a specific canonical URL
 
-ogp_site_url = f"https://canonical.com/data/mysql/docs/{version_slug}/"
+html_baseurl = f"https://canonical.com/data/mongodb/docs/{version_slug}/"
 
 # sphinx-sitemap uses html_baseurl to generate the full URL for each page:
 
