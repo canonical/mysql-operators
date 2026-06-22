1f471dfb624d3e3f3fa2789546b3bc91fe9a846a
Paulo Machado <paulo.machado@canonical.com>
2026-06-22 17:21:05 -0300

    [MISC] Fix full cluster recovery when async related (#332) (#379)
    * allow full cluster recovery when async related

--- kubernetes/src/charm.py
+++ kubernetes/src/charm.py
@@ -1103,8 +1103,14 @@ class MySQLOperatorCharm(MySQLCharmBase, TypedCharmBase[CharmConfig]):
             self.set_unit_status(MaintenanceStatus(member_state))
             return True
 
-        # avoid changing status while async replication is setting up
-        return not (self.replication_consumer.idle and self.replication_offer.idle)
+        # only assert for async replication state when online
+        if member_state == InstanceState.ONLINE and not (
+            self.replication_consumer.idle and self.replication_offer.idle
+        ):
+            logger.info("Skip status update when setting async replication")
+            return True
+
+        return False
 
     def _on_update_status(self, _: UpdateStatusEvent | None) -> None:
         """Handle the update status event."""
