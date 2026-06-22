16f960e8dbf92167bd9dea2afc90b1278faa758e
a-velasco <andreia.velasco@canonical.com>
2026-06-22 14:29:43 +0200

    configure sphinx for new canonical.com URL

--- docs/conf.py
+++ docs/conf.py
@@ -44,6 +44,7 @@ copyright = f"2025-{datetime.date.today().year}"
 
 html_title = project +  " 8.4" + " documentation"
 
+version_slug = f"{os.environ.get('READTHEDOCS_VERSION', 'local')}"
 
 # Documentation website URL
 #
@@ -52,7 +53,7 @@ html_title = project +  " 8.4" + " documentation"
 # NOTE: The Open Graph Protocol (OGP) enhances page display in a social graph
 #       and is used by social media platforms; see https://ogp.me/
 
-ogp_site_url = "https://canonical-charmed-mysql.readthedocs-hosted.com/"
+ogp_site_url = f"https://canonical.com/data/mongodb/docs/{version_slug}/"
 
 
 # Preview name of the documentation website
@@ -177,7 +178,7 @@ html_theme_options = {
 # TODO: If your documentation is hosted on https://docs.ubuntu.com/,
 #       uncomment and update as needed.
 
-# slug = ''
+slug = 'data/mysql/docs'
 
 #######################
 # Sitemap configuration: https://sphinx-sitemap.readthedocs.io/
@@ -185,7 +186,7 @@ html_theme_options = {
 
 # Use RTD canonical URL to ensure duplicate pages have a specific canonical URL
 
-html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
+ogp_site_url = f"https://canonical.com/data/mysql/docs/{version_slug}/"
 
 # sphinx-sitemap uses html_baseurl to generate the full URL for each page:
 
@@ -210,7 +211,7 @@ sitemap_excludes = [
 # Template and asset locations #
 ################################
 
-# html_static_path = ["_static"]
+html_static_path = ["_static"]
 templates_path = ["_templates"]
 
 
