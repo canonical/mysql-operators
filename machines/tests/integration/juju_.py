500d9629b8c56689e483a2d3f7af0d92012f8b0c
Paulo Machado <paulo.machado@canonical.com>
2026-07-07 17:07:13 -0300

    opsless integration test

--- machines/tests/integration/juju_.py
+++ machines/tests/integration/juju_.py
@@ -3,11 +3,9 @@
 
 import importlib.metadata
 
-import ops
-
 # libjuju version != juju agent version, but the major version should be identical—which is good
 # enough to check for secrets
 _libjuju_version = importlib.metadata.version("juju")
-has_secrets = ops.JujuVersion(_libjuju_version).has_secrets
 
 juju_major_version = int(_libjuju_version.split(".")[0])
+has_secrets = juju_major_version >= 3
