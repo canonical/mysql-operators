006f570cf3bf32a37db6d2e49b9e7bd797d1e0d9
Sinclert Pérez <sinclert.perez@canonical.com>
2026-08-18 09:54:04 +0200

    Define single-kernel libraries

---
+++ common/single_kernel/libs/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2026 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+from .data_platform_libs.v0.data_interfaces import (
+    DatabaseProvides,
+    DatabaseRequestedEvent,
+)
+
+from .grafana_agent.v0.cos_agent import COSAgentProvider
