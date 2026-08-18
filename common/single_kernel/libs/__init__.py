4cbf9bbf63164212a22aa77207d95b9e0a7ed729
Sinclert Pérez <sinclert.perez@canonical.com>
2026-08-18 09:54:04 +0200

    Define single-kernel libraries

---
+++ common/single_kernel/libs/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2026 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+from .data_platform_libs.v0.data_interfaces import (
+    DatabaseProviderData,
+    DatabaseProviderEventHandlers,
+    DatabaseRequestedEvent,
+)
