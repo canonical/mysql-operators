bf539825a586228075f528abdaf2be352f0523e8
Sinclert Pérez <sinclert.perez@canonical.com>
2026-08-20 15:30:22 +0200

    [K8s] Adapt backup tests

--- kubernetes/tests/integration/integration/helpers_backups.py
+++ kubernetes/tests/integration/integration/helpers_backups.py
@@ -1,9 +1,14 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import logging
 import shutil
 from contextlib import contextmanager
 from pathlib import Path
+
+from jubilant import Juju, TaskError
+
+logger = logging.getLogger(__name__)
 
 
 @contextmanager
@@ -14,3 +19,16 @@ def local_tmp_folder(name: str = "tmp"):
     tmp_folder.mkdir()
     yield tmp_folder
     shutil.rmtree(tmp_folder)
+
+
+def list_backups(juju: Juju, unit_name: str) -> list[str]:
+    """Lists backups in a safe manner (avoid raising if the action fails)."""
+    try:
+        logger.info("Listing existing backup ids")
+        task = juju.run(unit_name, "list-backups")
+    except TaskError:
+        return []
+
+    backups = task.results["backups"]
+    backups = [line.split("|")[0].strip() for line in backups.split("\n")[2:]]
+    return backups
