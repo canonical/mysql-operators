ee55fede5a8f7abb0bf8c34453176319b9bef67f
Sinclert Pérez <sinclert.perez@canonical.com>
2026-09-09 17:50:57 +0200

    Define single-kernel core

---
+++ common/single_kernel/core/constants.py
@@ -0,0 +1,28 @@
+# Copyright 2026 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+RELATION_DATABASE = "database"
+RELATION_LOGS_K8S = "logging"
+RELATION_LOGS_VM = "cos-agent"
+RELATION_PEERS = "database-peers"
+RELATION_OPS = "rolling-ops"
+RELATION_TLS_CLIENT = "client-certificates"
+RELATION_TLS_PEER = "peer-certificates"
+
+USERNAME_BACKUPS = "charmed-backup"
+USERNAME_MONITOR = "charmed-stats"
+USERNAME_OPERATOR = "charmed-operator"
+USERNAME_REPLICATION = "charmed-replication"
+
+ROLENAME_DBA = "charmed_dba"
+ROLENAME_DDL = "charmed_ddl"
+ROLENAME_DML = "charmed_dml"
+ROLENAME_READ = "charmed_read"
+ROLENAME_MONITOR = "charmed_stats"
+ROLENAME_BACKUPS = "charmed_backup"
+ROLENAME_ROUTER = "charmed_router"
+
+PASSWORD_BACKUPS_KEY = "backups-password"
+PASSWORD_MONITOR_KEY = "monitoring-password"
+PASSWORD_OPERATOR_KEY = "operator-password"
+PASSWORD_REPLICATION_KEY = "replication-password"
