1672b1faafa7cce26978cf6525de0609a7e9f0bb
Sinclert Pérez <sinclert.perez@canonical.com>
2026-09-09 17:50:57 +0200

    Define single-kernel core

---
+++ common/single_kernel/core/constants.py
@@ -0,0 +1,4 @@
+# Copyright 2026 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+RELATION_PEERS = "database-peers"
---
+++ common/single_kernel/core/literals.py
@@ -0,0 +1,7 @@
+# Copyright 2026 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+from typing import Literal
+
+RefreshPause = Literal["all", "first", "none"]
+Substrate = Literal["K8S", "VM"]
